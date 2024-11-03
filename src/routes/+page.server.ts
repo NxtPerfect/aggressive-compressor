61d0099e4e659c9f41470e3d95c15060d68c59b3
NxtPerfect <alakaxan@protonmail.com>
2024-11-03 13:02:57 +0100

    Added compressing file by server
    blocks sending source code files and exectuables

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,26 +1,38 @@
-// import { $ } from "bun";
+import { writeFile, readFile } from "node:fs/promises";
+import { execSync } from "node:child_process";
+import { createHash } from "node:crypto";
+import { env } from "$env/dynamic/private";
+import { fail } from "@sveltejs/kit";
 
 export const actions = {
-  upload: async ({ request }) => {
+  default: async ({ request }) => {
     const data = await request.formData();
-    // Since this is a request, it won't work
-    const file = data.get("uploadedFile") as File;
-    console.log(file)
-    // Save file
-    // await Bun.write(`./tmp/input/${file.name}`, file)
-    // console.log("We're in the server now");
-    // // tar -czf - ./tmp/input/${file.name} | bzip3 ./tmp/output/${noExtensionFileName}.tar.bz
-    // const [noExtensionFileName, fileExtension] = file.name.strip(".")
-    //
-    // const bannedExtensions = ["js", "ts", "zip", "7z", "tar", "tar.gz", "tar.bz", "exe", "sh", "bat"]
-    // if (bannedExtensions.includes(fileExtension)) {
-    //   console.error("Tried to upload banned extension file.")
-    //   return
-    // }
-    // await $`tar -czf - ./tmp/input/${file.name} | bzip3 ./tmp/output/${noExtensionFileName}.tar.bz`
+    const uploadFile = data.get("uploadedFile") as File;
+    console.log(uploadFile)
+
+    const extension = uploadFile.name.split(".")[1]
+    const bannedExtensions = ["js", "ts", "zip", "7z", "tar", "exe", "sh", "bat"]
+    if (bannedExtensions.includes(extension)) {
+      console.error("Tried to upload banned extension file.")
+      return fail(500, { message: "Tried to upload banned file type." })
+    }
+
+    const hashInputFile = computeHash(uploadFile.name)
+    console.log("Hash of file", hashInputFile)
+    const inputPath = "tmp/input/" + hashInputFile + "." + extension
+    const outputPath = "tmp/output/" + hashInputFile + ".tar.bz3"
+    await writeFile(inputPath, Buffer.from(await uploadFile.arrayBuffer()))
+    const {error, stdout, stderr} = execSync(`touch ${outputPath} & tar -cvf - ${inputPath} | bzip3 -j 16 > ${outputPath}`)
+    if (error) {
+      console.error(error)
+      return fail(500, { message: "Failed to compress file." })
+    }
+    const output = await readFile(outputPath)
+    console.log(output)
+    return { success: true, file: output}
   }
 }
 
-function computeHash(fileName) {
-  return -1
+function computeHash(fileName: string) {
+  return createHash("sha1").update(fileName + env.SECRET).digest('hex')
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,24 @@
-<main class="bg-black pt-52 h-[100svh] flex flex-col justify-self-center content-center items-center">
-  <h1 class="text-white">Welcome to <span class="text-lg font-mono text-red-500 shadow-black animate-pulse duration-500">Aggressive Compressor</span></h1>
+<main class="text-white bg-black pt-52 min-h-[100svh] flex flex-col justify-self-center content-center items-center">
+  <h1 class="text-white text-2xl">Welcome to <span class="font-mono text-red-500 shadow-black animate-pulse duration-500 text-4xl tracking-wide">Aggressive Compressor</span></h1>
 
-  <form method="POST" action="?/upload" class="flex flex-col gap-2 w-fit items-center mt-16">
+  <form method="POST" enctype="multipart/form-data" class="flex flex-col gap-2 w-fit items-center mt-16">
     <input type="file" id="uploadedFile" name="uploadedFile" class="text-white" accept="video/*, audio/*, text/*, image/*, .md, .json" required/>
     <button class="bg-white px-2 py-1 text-black w-fit rounded-md hover:bg-neutral-600 active:bg-neutral-400" type="submit">Upload file</button>
   </form>
+
+  <h2 class="mt-64 text-4xl">FAQ</h2>
+  <div class="flex flex-col mx-24 gap-16">
+    <div class="grid grid-cols-2 gap-16 min-h-[6lh] justify-center justify-items-center items-center self-center content-center">
+      <h3 class="text-2xl">What?</h3>
+      <p>It's a website to compress any non-executable and non-code file to as small size as possible. Perhaps you need to send it through an email, or you simply have bad bandwidth, or the receipant. Aggressive Compressor lets you solve these issues with easy of use web interface.</p>
+    </div>
+    <div class="grid grid-cols-2 gap-16 min-h-[6lh] justify-items-center items-center content-center">
+      <h3 class="text-2xl">How?</h3>
+      <p>Using Linux magic and a bit of celestial dust, we can achieve the best compression any other website on this domain has ever done.</p>
+    </div>
+    <div class="grid grid-cols-2 gap-16 min-h-[6lh] justify-items-center items-center content-center">
+      <h3 class="text-2xl">Why?</h3>
+      <p>Aggressive Compressor is a hobby project created from a simple idea, how much can you really compress a file? How corrupt would the data be? What is the limit of compression, before being unusable? All of these questions this project answers</p>
+    </div>
+  </div>
 </main>
