b8aefac2ece49aad1918258416873e23722cc17a
NxtPerfect <alakaxan@protonmail.com>
2024-11-03 13:21:22 +0100

    Created orm sqlite tables for users and files

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,17 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
-    id: integer('id').primaryKey(),
-    age: integer('age')
+    email: text('email').primaryKey().unique().default("not@set.yet").notNull(),
+    password: text('password').default("change_me"),
+    subscriptioneExpirationDate: text('subscriptioneExpirationDate').default("NONE"),
+    totalBandwidthLeftInGB: integer('totalBandwidthLeftInGB').default(0)
+});
+
+export const files = sqliteTable('files', {
+    hash: text('fileHash').primaryKey().unique().notNull(),
+    originalPath: text('originalPath').notNull(),
+    sourcePath: text('sourcePath').notNull(),
+    rawSizeBytes: integer('rawSize').default(0).notNull(),
+    compressedSizeBytes: integer('compressedSize').default(999),
+    howManyTimesUploaded: integer('howManyTimesUploaded').default(1)
 });
